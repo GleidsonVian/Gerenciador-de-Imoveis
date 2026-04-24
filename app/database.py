f54aeacc1c50c1a0fd9b4004e27c1e5697193670
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2026-04-24 02:04:20 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv()  # funciona local, no Railway usa as variáveis de ambiente direto
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL") or os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,4 +17,4 @@ def get_db():
     try:
         yield db
     finally:
-        db.close
+        db.close()
